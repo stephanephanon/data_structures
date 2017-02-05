da5a53f15a57877f634500a97e2b7c72cef55404
Stephanie Sydorko <stephanephanon@gmail.com>
2017-02-05 14:24:51 -0500

    add listADT and unit tests

--- list.py
+++ list.py
@@ -1,14 +1,25 @@
-from abc import ABCMeta, abstractmethod
+from abc import ABCMeta, abstractmethod, abstractproperty
 
 
 # --------------------------------
 # List APIs
 # --------------------------------
+from linked_lists import LinkedList, Node
+
+
+class ListException(Exception):
+    """
+    ListABC should raise ListException for list errors such as:
+    1. no element for first
+    2. wrong class type for ordered list
+    3. no element to remove
+    """
+
 class ListABC(metaclass=ABCMeta):
     """
     The ListABC provides the api for a basic list data structure
     """
-    @abstractmethod
+    @abstractproperty
     def first(self):
         """
         Examine the element at the front of the list
@@ -16,7 +27,7 @@ class ListABC(metaclass=ABCMeta):
         """
         pass
 
-    @abstractmethod
+    @abstractproperty
     def last(self):
         """
         Examine the element at the rear of the list
@@ -25,7 +36,7 @@ class ListABC(metaclass=ABCMeta):
         pass
 
     @abstractmethod
-    def remove_firt(self):
+    def remove_first(self):
         """
         Remove and return the element at the front of the list
         :return: element at front
@@ -49,7 +60,7 @@ class ListABC(metaclass=ABCMeta):
         """
         pass
 
-    @abstractmethod
+    @abstractproperty
     def is_empty(self):
         """
         Return True if list is empty, else False
@@ -57,7 +68,7 @@ class ListABC(metaclass=ABCMeta):
         """
         pass
 
-    @abstractmethod
+    @abstractproperty
     def size(self):
         """
         Return the size (number of elements) in the list
@@ -110,3 +121,119 @@ class UnorderedListABC(ListABC, metaclass=ABCMeta):
 # --------------------------------
 # Ordered List Implementation
 # --------------------------------
+class OrderedLinkedList(OrderedListABC):
+    """
+    The OrderedLinkedList class implements a linked list
+    that will insert elements based on their ordering.
+    It expects that elements are of the same type and
+    order-comparable.
+    An order-comparable class defines
+        __eq__, __ne__, __lt__, __le__, __gt__, __ge__
+
+    """
+    def __init__(self, element_class):
+        """
+        :param element_class: class that this linked list will store
+        """
+        self.element_class = element_class
+        self._list = LinkedList()
+
+    def __iter__(self):
+        for obj in self._list:
+            yield obj
+
+    def add(self, x):
+        """
+        Add an element into the list at its appropriate spot
+        """
+        if not isinstance(x, self.element_class):
+            raise ListException("Cannot add element because is not an instance of {}"
+                                .format(self.element_class.__name__))
+
+        # find where the element belongs in the order
+        # base case, empty list
+        if len(self._list) == 0:
+            self._list.insert_first(x)
+        # > 0 element
+        else:
+            # loop until we hit a bigger item then insert
+            prev_node = None
+            node = self._list.first
+            while node:
+                if node.element > x:
+                    break
+                prev_node = node
+                node = node.next_node
+
+            if prev_node is None:
+                # first item was greater than x
+                self._list.insert_first(x)
+            else:
+                self._list.insert_after_node(prev_node, x)
+
+        return
+
+    @property
+    def first(self):
+        """
+        Examine the element at the front of the list
+        :return: element at front
+        """
+        # if there is nothing in the list then raise an exception
+        if self.is_empty:
+            raise ListException("Cannot fetch first element from empty list")
+        return self._list.first.element
+
+    @property
+    def last(self):
+        """
+        Examine the element at the rear of the list
+        :return: element at rear
+        """
+        # if there is nothing in the list then raise an exception
+        if self.is_empty:
+            raise ListException("Cannot fetch last element from empty list")
+        return self._list.last.element
+
+    def __len__(self):
+        """
+        Return the length of the list
+        :return: number of elements
+        """
+        return len(self._list)
+
+    @property
+    def size(self):
+        """
+        Return the number of elements in the list
+        :return:
+        """
+        return len(self._list)
+
+    def remove_first(self):
+        # if there is nothing in the list then raise an exception
+        if self.is_empty:
+            raise ListException("Cannot remove first from empty list")
+        return self._list.delete_first()
+
+    def remove_last(self):
+        # if there is nothing in the list then raise an exception
+        if self.is_empty:
+            raise ListException("Cannot remove last from empty list")
+        return self._list.delete_last()
+
+    @property
+    def is_empty(self):
+        """
+        Return true if list is empty else False
+        :return: True or False
+        """
+        return True if self.size == 0 else False
+
+    def contains(self, x):
+        """
+        Return True if list contains element x, else False
+        :param x: object to check for
+        :return: True if in list, else False
+        """
+        return self._list.search(x)
--- tests/test_list.py
+++ tests/test_list.py
@@ -1,1 +1,176 @@
-__author__ = 'ssydorko'
+from unittest import TestCase
+from list import OrderedLinkedList, ListException
+
+
+class OrderedLinkedListTests(TestCase):
+    def test_mixed_classes(self):
+        """
+        Test that we raise an exception if the element class
+        does not define the methods needed for ordering
+        """
+        class A(object):
+            def __init__(self):
+                self.x = 7
+
+        l = OrderedLinkedList(A)
+        with self.assertRaises(ListException):
+            l.add(7)
+
+    def test_first(self):
+        l = OrderedLinkedList(int)
+
+        # empty list.
+        self.assertEqual(l.size, 0)
+        with self.assertRaises(ListException):
+            l.first
+
+        # add an item
+        l.add(9)
+        ret = l.first
+        self.assertEqual(l.size, 1)
+        self.assertEqual(9, ret)
+        self.assertEqual(l.size, 1)
+
+        # add two elements
+        l.add(99)
+        self.assertEqual(l.size, 2)
+        ret = l.first
+        self.assertEqual(9, ret)
+        self.assertEqual(l.size, 2)
+
+    def test_last(self):
+        l = OrderedLinkedList(int)
+
+        # empty list.
+        self.assertEqual(l.size, 0)
+        with self.assertRaises(ListException):
+            l.last
+
+        # add an item
+        l.add(9)
+        ret = l.last
+        self.assertEqual(9, ret)
+
+        # add two elements
+        l.add(99)
+        ret = l.last
+        self.assertEqual(99, ret)
+
+    def test_remove_first(self):
+        l = OrderedLinkedList(int)
+
+        # no items - remove
+        with self.assertRaises(ListException):
+            ret = l.remove_first()
+
+        # 1 item - remove
+        l.add(4)
+        self.assertEqual(l.size, 1)
+        ret = l.remove_first()
+        self.assertEqual(ret, 4)
+        self.assertEqual(l.size, 0)
+
+        # more than 1 item - remove
+        l.add(6)
+        l.add(8)
+        self.assertEqual(l.size, 2)
+        ret = l.remove_first()
+        self.assertEqual(ret, 6)
+        self.assertEqual(l.size, 1)
+
+    def test_remove_last(self):
+        l = OrderedLinkedList(int)
+
+        # no items - remove
+        with self.assertRaises(ListException):
+            ret = l.remove_last()
+
+        # 1 item - remove
+        l.add(4)
+        self.assertEqual(l.size, 1)
+        ret = l.remove_last()
+        self.assertEqual(ret, 4)
+        self.assertEqual(l.size, 0)
+
+        # more than 1 item - remove
+        l.add(6)
+        l.add(8)
+        self.assertEqual(l.size, 2)
+        ret = l.remove_last()
+        self.assertEqual(ret, 8)
+        self.assertEqual(l.size, 1)
+
+    def test_contains(self):
+        l = OrderedLinkedList(int)
+
+        # empty list
+        self.assertFalse(l.contains(4))
+
+        # populated list
+        l.add(3)
+        l.add(2)
+        l.add(5)
+
+        # in the list
+        self.assertTrue(l.contains(5))
+
+        # not in the list
+        self.assertFalse(l.contains(9))
+
+    def test_is_empty(self):
+        l = OrderedLinkedList(int)
+
+        # empty list
+        self.assertTrue(l.is_empty)
+
+        # 1 item
+        l.add(3)
+        self.assertFalse(l.is_empty)
+
+        # multiple items
+        l.add(2)
+        l.add(4)
+        self.assertFalse(l.is_empty)
+
+    def test_size(self):
+        l = OrderedLinkedList(int)
+
+        # empty list
+        self.assertEqual(l.size, 0)
+
+        # 1 item
+        l.add(3)
+        self.assertEqual(l.size, 1)
+
+        # multiple items
+        l.add(2)
+        l.add(4)
+        self.assertEqual(l.size, 3)
+
+    def test_add_smaller_than_first(self):
+        l = OrderedLinkedList(int)
+        l.add(9)
+        l.add(1)
+        self.assertEqual(l.first, 1)
+        self.assertEqual(l.last, 9)
+
+    def test_add_bigger_than_last(self):
+        l = OrderedLinkedList(int)
+        l.add(3)
+        l.add(4)
+        self.assertEqual(l.first, 3)
+        self.assertEqual(l.last, 4)
+        l.add(9)
+        self.assertEqual(l.first, 3)
+        self.assertEqual(l.last, 9)
+
+    def test_add_mixed_order(self):
+        l = OrderedLinkedList(int)
+        l.add(2)
+        l.add(4)
+        l.add(1)
+        l.add(3)
+        l.add(5)
+        expected_ret = [1, 2, 3, 4, 5]
+        ret = [i for i in l]
+        self.assertEqual(expected_ret, ret)
